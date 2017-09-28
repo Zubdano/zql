7d5f1c47487911b2ca40aeab358362a4d5e323e7
Wojtek Swiderski <wswiders@uwaterloo.ca>
2017-09-28 19:07:51 -0400

    Mock annotation endpoint

--- mock_backend/server.py
+++ mock_backend/server.py
@@ -1,6 +1,45 @@
-from flask import Flask, jsonify
+import json
+
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 
-@app.route("/")
+
+VERB_TOKENS = frozenset(['am', 'eat'])
+ADJ_TOKENS = frozenset(['funny', 'yellow'])
+
+
+@app.route('/')
 def hello():
     return jsonify({'yo': 'face'})
+
+
+@app.route('/annotation/', methods=['POST'])
+def annotation():
+	"""
+	Takes in an object representing a sentence and returns annotations for the words in that
+	sentence.
+	"""
+	sentence = json.loads(request.data)
+	sentence['errorMsg'] = None
+	sentence['isError'] = False
+
+	for word in sentence['words']:
+		if word['text'] in VERB_TOKENS:
+			word['annotation'] = 'verb'
+		elif word['text'] in ADJ_TOKENS:
+			word['annotation'] = 'adj'
+		else:
+			word['annotation'] = None
+
+		if word['text'] == 'cance':
+			sentence['errorMsg'] = 'i said no cance'
+			sentence['isError']	= True
+
+	if not sentence['isError'] and sentence['words'] and \
+			sentence['words'][-1]['text'].endswith('.'):
+		sentence['isComplete'] = True
+	else:
+		sentence['isComplete'] = False
+
+	return jsonify(sentence)
+
