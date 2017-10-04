79462f858475fb88b3a45eb527316d6372e9b73a
Wojtek Swiderski <wswiders@uwaterloo.ca>
2017-10-04 00:30:50 -0400

    editorstate -> fetch annotation, requirements for backend

--- zql-frontend/src/TextInput.js
+++ zql-frontend/src/TextInput.js
@@ -1,42 +1,27 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
+import { Editor } from 'draft-js';
 
 import './TextInput.css';
-import { fetchAnnotation, updateSentenceText } from './reducer';
+import { fetchAnnotation, setEditorState } from './reducer';
 
 
 class TextInput extends Component {
-
-  handleChange = event => {
-    console.log(`input changed to '${event.target.value}'`);
-    this.props.updateSentenceText(event.target.value);
-    this.props.fetchAnnotation(this.props.sentence);
-  };
-
-  handleClick = () => {
-    console.log(this.state.sentence);
-  };
-
-  getText() {
-    return this.props.sentence.words.map(word => word.text).join(' ');
+  handleChange = editorState => {
+    this.props.fetchAnnotation(editorState.getCurrentContent().getPlainText(' '));
+    this.props.setEditorState(editorState);
   }
 
 	render() {
-
-    console.log(this.props);
-    // TODO: Make textbox and button pretty
     return (
       <div>
-        <textarea
-          className="TextInput"
-          type="text"
+        <Editor
+          editorState={this.props.editorState}
           onChange={this.handleChange}
         />
-        <button onClick={this.handleClick}>Submit</button>
-        <div>{this.getText()}</div>
       </div>
     );
 	}
 }
 
-export default connect(state => state, {fetchAnnotation, updateSentenceText})(TextInput);
+export default connect(state => state, {fetchAnnotation, setEditorState})(TextInput);
--- zql-frontend/src/reducer.js
+++ zql-frontend/src/reducer.js
@@ -1,68 +1,58 @@
+import { EditorState } from 'draft-js';
+
 import Requestor from './requests/requestor';
 
 const BASE_URL = 'http://localhost:5000';
 const ANNOTATION_ROUTE = '/annotation/';
 
 const RECEIVE_ANNOTATION = 'RECEIVE_ANNOTATION';
-const UPDATE_WORD_TEXT = 'UPDATE_WORD_TEXT';
+const UPDATE_EDITOR_STATE = 'UPDATE_EDITOR_STATE';
 
 const initialState = {
-  sentence: {
-    words: [],
-    errorMsg: null,
-    isComplete: false,
-    isError: false,
-  },
+  editorState: EditorState.createEmpty(),
 };
 
 // TODO: Rename
 function reducer(state = initialState, action) {
   switch (action.type) {
     case RECEIVE_ANNOTATION:
-      // TODO: Finish
+      console.log(action.data);
+      return state;
+    case UPDATE_EDITOR_STATE:
       return {
         ...state,
-        sentence: action.sentence,
-      };
-    case UPDATE_WORD_TEXT:
-      // TODO: copy old annotations
-      return {
-        ...state,
-        sentence: {
-          ...state.sentence,
-          words: action.words.map(wordText => ({text: wordText})),
-        }
+        editorState: action.editorState,
       };
     default:
       return state;
   }
 }
 
-function updateWordText(words) {
+function updateEditorState(editorState) {
   return {
-    type: UPDATE_WORD_TEXT,
-    words: words,
+    type: UPDATE_EDITOR_STATE,
+    editorState: editorState,
   };
 }
 
-function updateSentenceText(text) {
-  return (dispatch) => dispatch(updateWordText(text.split(' ')));
+function setEditorState(editorState) {
+  return (dispatch) => dispatch(updateEditorState(editorState));
 }
 
-function receiveAnnotation(sentence) {
+function receiveAnnotation(data) {
   return {
     type: RECEIVE_ANNOTATION,
-    sentence: sentence,
+    data: data,
   };
 }
 
-function fetchAnnotation(sentence) {
-  return (dispatch) => new Requestor(BASE_URL).post(ANNOTATION_ROUTE, sentence)
+function fetchAnnotation(plainText) {
+  return (dispatch) => new Requestor(BASE_URL).post(ANNOTATION_ROUTE, {raw: plainText})
     .then(json => dispatch(receiveAnnotation(json)));
 }
 
 export {
   fetchAnnotation,
-  updateSentenceText,
   reducer,
+  setEditorState,
 };
