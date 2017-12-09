9afd6e8d13682d97d84b9ddc8a7ebfed7f187c06
Pranav Anand <pranavanand@Pranavs-MacBook-Pro.local>
2017-12-09 12:15:51 -0500

    seed changes

--- scripts/seed.py
+++ scripts/seed.py
@@ -3,9 +3,9 @@ import requests
 submissions = []
 
 
-requests.post('http://localhost:2420/create_user', json={'username': 'editor', 'password': 'minecraft', 'permission': 0})
-requests.post('http://localhost:2420/create_user', json={'username': 'writer', 'password': 'minecraft', 'permission': 1})
-requests.post('http://localhost:2420/create_user', json={'username': 'reader', 'password': 'minecraft', 'permission': 2})
+# requests.post('http://localhost:2420/create_user', json={'username': 'editor', 'password': 'minecraft', 'permission': 0})
+# requests.post('http://localhost:2420/create_user', json={'username': 'writer', 'password': 'minecraft', 'permission': 1})
+# requests.post('http://localhost:2420/create_user', json={'username': 'reader', 'password': 'minecraft', 'permission': 2})
 
 for i in range(10):
     submissions.extend([
@@ -14,4 +14,5 @@ for i in range(10):
     ])
 
 for submission in submissions:
-    requests.post('http://localhost:2015/event', json={'raw': submission})
+    r = requests.post('http://localhost:2015/event', json={'raw': submission}, headers={'User.Username': 'vasan', 'User.Permission': '0'})
+    print(r.content)
